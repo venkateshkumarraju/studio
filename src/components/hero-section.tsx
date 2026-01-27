9dad1d1a461ba66175bbbd16a40fc485852f4354
Venkatesh Kumar <raju.venkateshkumar@gmail.com>
2026-01-27 11:40:13 +0000

    remove this (_for element a_)

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -27,9 +27,6 @@ export function HeroSection() {
             </p>
 
             <div className="flex flex-col gap-4 sm:flex-row">
-              <a href="https://drive.google.com/file/d/1oWn_64W76GiRneAXl4QUQrU4vsmV6FAb/view?usp=drive_link" download className="learn-more">
-                  View Resume <ArrowDown className="ml-2 h-4 w-4" />
-              </a>
               <Link href="#contact" className="learn-more">
                 Hire me
               </Link>
