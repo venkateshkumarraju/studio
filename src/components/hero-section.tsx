f4e2c32f6ee48775b3bcdd1307f3fd51b561b745
Venkatesh Kumar <raju.venkateshkumar@gmail.com>
2026-01-27 11:33:03 +0000

    <div className="flex flex-col gap-4 sm:flex-row">               <Button

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -27,11 +27,9 @@ export function HeroSection() {
             </p>
 
             <div className="flex flex-col gap-4 sm:flex-row">
-              <Button asChild size="lg" variant="secondary">
-                <a href="https://github.com/venkateshkumarraju/venkateshkumarraju/blob/main/assets/venkatesh_kumar_raju.pdf" download>
+              <a href="https://github.com/venkateshkumarraju/venkateshkumarraju/blob/main/assets/venkatesh_kumar_raju.pdf" download className="learn-more">
                   View Resume <ArrowDown className="ml-2 h-4 w-4" />
-                </a>
-              </Button>
+              </a>
               <Link href="#contact" className="learn-more">
                 Hire me
               </Link>
